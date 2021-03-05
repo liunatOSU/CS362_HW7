445a6ac548fae96f9b9d10094d9d1f5627651a4c
Nathan Liu <liunat@flip3.engr.oregonstate.edu>
2021-03-05 15:44:46 -0800

    Wrote third test, numbers taht are a multiple of 5 should print 'Buzz'

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,10 +2,11 @@ import unittest
 from fizzbuzz import fizzbuzz
 
 class TestFizzbuzz(unittest.TestCase):
-  def test_fizzBuzzInvalid(self):
+  def test_fizzbuzzInvalid(self):
     self.assertEqual(fizzbuzz(-1), None)
-  def test_fizzBuzz3(self):
+  def test_fizzbuzz3(self):
     self.assertEqual(fizzbuzz(3), "Fizz")
-  
+  def test_fizzbuzz(self):
+    self.assertEqual(fizzbuzz(5), "Buzz")
 if __name__ == "__main__":
     unittest.main()
