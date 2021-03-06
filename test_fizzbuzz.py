f8194d5cfc3eeee2f0e08b39255651f6dcfec845
Nathan Liu <liunat@flip3.engr.oregonstate.edu>
2021-03-05 16:01:03 -0800

    Wrote fourth test, numbers that are a multiple of 3 and 5 should print 'FizzBuzz'

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,7 +6,9 @@ class TestFizzbuzz(unittest.TestCase):
     self.assertEqual(fizzbuzz(-1), None)
   def test_fizzbuzz3(self):
     self.assertEqual(fizzbuzz(3), "Fizz")
-  def test_fizzbuzz(self):
+  def test_fizzbuzz5(self):
     self.assertEqual(fizzbuzz(5), "Buzz")
+  def test_fizzbuzz15(self):
+    self.assertEqual(fizzbuzz(15), "FizzBuzz")
 if __name__ == "__main__":
     unittest.main()
