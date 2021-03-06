bfa69906fd487272d5de5339e97106b49c649b19
Nathan Liu <liunat@flip3.engr.oregonstate.edu>
2021-03-05 16:02:36 -0800

    Implemented fizzbuzz method so numbers that are a multiple of 3 and 5 print 'FizzBuzz'

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 def fizzbuzz(num):
   if num < 0 or num > 100:
     return None
+  if num % 3 == 0 and num % 5 == 0:
+    return "FizzBuzz"
   if num % 3 == 0:
     return "Fizz"
   if num % 5 == 0:
