c3b9ef0e6e736132280a652e32f4f27d5c23e4c1
Nathan Liu <liunat@flip3.engr.oregonstate.edu>
2021-03-05 17:49:34 -0800

    Wrote second test, years that are divisible by 4, but not by 100 should return True

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,6 +4,8 @@ from leapyear import leapyear
 class TestLeapYear(unittest.TestCase):
   def test_leapyear5(self):
     self.assertEqual(leapyear(5), False)
+  def test_leapyear4(self):
+    self.assertEqual(leapyear(4), True)
     
 if __name__ == "__main__":
     unittest.main()
